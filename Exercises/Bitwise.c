8f6349d3bd4fa5c4585aa6762a6eb141379ba12d
oscarlr16 <oscarleon.ro16@gmail.com>
2022-08-01 17:25:11 -0500

    The program returns the maximum of the combinations

--- Exercises/Bitwise.c
+++ Exercises/Bitwise.c
@@ -1,23 +1,54 @@
 #include<stdio.h>
 
-void combinations(int num);
+void MAX_AND(int num);
+void MAX_OR(int num);
+void MAX_XOR(int num);
 
 void main(void){
     int num=NULL;
     printf("Ingrese un numero: \n");
     scanf("%d",&num);
-    combinations(num);
+    MAX_AND(num);
+    MAX_OR(num);
+    MAX_XOR(num);
 }
 
-
-
-void combinations(int num){
-    int a=0;
+void MAX_AND(int num){
+    int a=0,max=0;
     for (int i = 1; i <= num; i++)
     {
         for(int j = num-i; j>0;j--){
-            printf("%d\t",j);
-            printf("%d\n",j+i);
+            if((j&(j+1))>=max){
+                max = j&(j+1);
+            }
         }
     }
+    printf("%d\n",max);
+}
+
+void MAX_OR(int num){
+    int a=0,max=0;
+    for (int i = 1; i <= num; i++)
+    {
+        for(int j = num-i; j>0;j--){
+            if((j|(j+1))>=max){
+                max = j|(j+1);
+            }
+        }
+    }
+    printf("%d\n",max);
+}
+
+void MAX_XOR(int num){
+    int a=0,max=0;
+    for (int i = 1; i <= num; i++)
+    {
+        for(int j = num-i; j>0;j--){
+            if((j^(j+1))>=max){
+                max = j^(j+1);
+            }
+        }
+ 
+    }
+    printf("%d\n",max);
 }
