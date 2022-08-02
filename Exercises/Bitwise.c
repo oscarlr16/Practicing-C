27013f9df6cb4c3e4495f25fc9624598d46b850b
oscarlr16 <oscarleon.ro16@gmail.com>
2022-08-02 12:55:54 -0500

    Fixed a bug. Changed j+1 to j+i.

--- Exercises/Bitwise.c
+++ Exercises/Bitwise.c
@@ -1,51 +1,51 @@
 #include<stdio.h>
 
-void MAX_AND(int num);
-void MAX_OR(int num);
-void MAX_XOR(int num);
+void MAX_AND(int num, int k);
+void MAX_OR(int num, int k);
+void MAX_XOR(int num, int k);
 
 void main(void){
-    int num=NULL;
+    int num=NULL,k=NULL;
     printf("Ingrese un numero: \n");
-    scanf("%d",&num);
-    MAX_AND(num);
-    MAX_OR(num);
-    MAX_XOR(num);
+    scanf("%d%d",&num,&k);
+    MAX_AND(num, k);
+    MAX_OR(num, k);
+    MAX_XOR(num, k);
 }
 
-void MAX_AND(int num){
-    int a=0,max=0;
+void MAX_AND(int num, int k){
+    int max=0;
     for (int i = 1; i <= num; i++)
     {
         for(int j = num-i; j>0;j--){
-            if((j&(j+1))>=max){
-                max = j&(j+1);
+            if((j&(j+i))>=max && (j&(j+i))<k){
+                max = j&(j+i);
             }
         }
     }
     printf("%d\n",max);
 }
 
-void MAX_OR(int num){
-    int a=0,max=0;
+void MAX_OR(int num, int k){
+    int max=0;
     for (int i = 1; i <= num; i++)
     {
         for(int j = num-i; j>0;j--){
-            if((j|(j+1))>=max){
-                max = j|(j+1);
+            if((j|(j+i))>=max && (j|(j+i))<k){
+                max = j|(j+i);
             }
         }
     }
     printf("%d\n",max);
 }
 
-void MAX_XOR(int num){
-    int a=0,max=0;
+void MAX_XOR(int num, int k){
+    int max=0;
     for (int i = 1; i <= num; i++)
     {
         for(int j = num-i; j>0;j--){
-            if((j^(j+1))>=max){
-                max = j^(j+1);
+            if((j^(j+i))>=max && (j^(j+i))<k){
+                max = j^(j+i);
             }
         }
  
